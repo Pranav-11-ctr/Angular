5b735025dbcd671a858f1dfc99811c002091a354
Pranav1110 <murarikmrmadhubani11@gmail.com>
2022-05-18 09:55:16 +0530

    Dynamic binding or style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,15 @@ import { disableDebugTools } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Loop Case';
-  users=['Pranav','Prabhat','Subham','Rohit','Rahul'];
-  userList=[
-    {name:'Pranav',Roll:83,Branch:'CSE'},
-    {name:'Prahat',Roll:94,Branch:'Cse'},
-    {name:'Sumit',Roll:20,Branch:'Aeie'}
-  ]
-  studentList=[
-    {name:'Pranav',Roll:83,Language:['c','c++','java']},
-    {name:'Prabhat',Roll:85,Language:['R','C','c++']},
-    {name:'Rohit',Roll:70,Language:['C++',"java"]}
-  ]
+  title = 'Style Binding';
+  color='blue';
+  bgcolor='grey'
+  upButton()
+  {
+    this.color='pink'
+    this.bgcolor='red'
+  }
+  
  
   
 
